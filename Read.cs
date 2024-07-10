322c72b5302f20d137ac3af40f9bfefeccc9e3a8
Disha711 <153199301+Disha711@users.noreply.github.com>
2024-07-10 11:19:10 +0530

    Add files via upload

--- Read.cs
+++ Read.cs
@@ -1,14 +1,14 @@
 class Read
 {
-	public static void Main(string[] args)
-	{
-		double num=System.Convert.ToDouble(args[0]);
-		double num1=System.Convert.ToDouble(args[1]);
+    public static void Main(string[] args)
+    {
+        double num = System.Convert.ToDouble(args[0]);
+        double num1 = System.Convert.ToDouble(args[1]);
 
-		//System.Console.WriteLine("i:{smallInt}, ii{largeInt}, iii Not less than{smallInt}, iv: Not greater than{largeInt}");
-		System.Console.WriteLine("Min Value:{0}", System.Math.Min(num, num1));
-		System.Console.WriteLine("Max Value:{0}", System.Math.Max(num, num1));
-		System.Console.WriteLine("Not Less Than:{0}", System.Math.Min(num, num1));
-		System.Console.WriteLine("Not Greater Than:{0}", System.Math.Max(num, num1));
-	}
+        System.Console.Write("(i)Min Value:{0} (ii)Max Value:{1} (iii)Not Less Than:{2} (iv)Not Greater Than:{3}", 
+                             System.Math.Min(num, num1), 
+                             System.Math.Max(num, num1), 
+                             System.Math.Min(num, num1), 
+                             System.Math.Max(num, num1));
+    }
 }
